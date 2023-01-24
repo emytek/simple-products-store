c33832dbfa6a25f7c556cba6778f1a02ccc09f30
emytek <66952283+emytek@users.noreply.github.com>
2023-01-24 16:21:12 +0100

    MongoDB mongoose connection + Creating the User model and routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const http = require('http');
+require('./connection')
 
 const server = http.createServer(app);
 const {Server} = require('socket.io');
@@ -10,10 +11,13 @@ const io = new Server(server, {
     methods: ['GET', 'POST', 'PATCH', "DELETE"]
 })
 
+const User = require('./models/User');
+const userRoutes = require('./routes/userRoutes');
+
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
+app.use('/users', userRoutes);
 
 server.listen(8080, ()=> {
     console.log('server running at port', 8080)
