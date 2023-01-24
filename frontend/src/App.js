1fb4358b9888bbceecc8455ed65458eb40f49c63
emytek <66952283+emytek@users.noreply.github.com>
2023-01-24 11:51:08 +0100

    Setting up the Login and the Signup display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route} from 'react-router-dom'
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route index element={<Home />} />
         <Route path="*" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   );
