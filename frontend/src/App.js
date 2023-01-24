3bc66c974de822b6d97a29b6009f0eb5d541817d
emytek <66952283+emytek@users.noreply.github.com>
2023-01-24 18:12:45 +0100

    State management with redux toolkit: Creating the store, reducers and slices

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -9,13 +9,15 @@ import Signup from './pages/Signup';
 function App() {
   return (
     <div>
+    <BrowserRouter>
         <Navigation />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="*" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="*" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from "./store";
+// import { PersistGate } from 'redux-persist/lib/integration/react';
+// import persistStore from 'redux-persist/es/persistStore';
 
+// const persistedStore = persistStore
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    {/* <PersistGate loading={<div>Loading...</div>} persistor={persistedStore}></PersistGate> */}
+      <App />
+  </Provider>
 );
 
 
